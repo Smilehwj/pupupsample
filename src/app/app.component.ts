05a1fa5012c20e505b199673aa5dccc2c018e83c
Smilehwj <937761969@qq.com>
2018-05-29 21:19:30 +0800

    angular elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { NgElementConstructor, createCustomElement } from '@angular/elements';
+import { PopupService } from './popup/popup.service';
+import { PopupComponent } from './popup/popup.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <input #input value="Message">
+    <button (click)="popup.showAsComponent(input.value)">
+        Show as component </button>
+    <button (click)="popup.showAsElement(input.value)">
+        Show as element </button>
+  `
 })
+
 export class AppComponent {
-  title = 'app';
+  constructor(private injector: Injector, public popup: PopupService) {
+    // on init, convert PopupComponent to a custom element
+    const PopupElement = createCustomElement(PopupComponent, { injector: this.injector });
+    // register the custom element with the browser.
+    customElements.define('popup-element', PopupElement);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PopupComponent } from './popup/popup.component';
+import { PopupService } from './popup/popup.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, PopupComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
+  providers: [PopupService],
+  bootstrap: [AppComponent],
+  entryComponents: [PopupComponent],
 })
 export class AppModule { }
